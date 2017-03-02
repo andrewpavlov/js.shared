b9cfe875b511dfb8ee772e6785b8b0a9cdbaecad
Andrey Pavlov <andrew.m.pavlov@gmail.com>
2017-03-02 10:09:17 +0300

    request vars support

--- index.js
+++ index.js
@@ -3,5 +3,6 @@
 exports = module.exports = {
     utils: require('./utils'),
     config: require('./config'),
-    cookies: require('./cookies')
+    cookies: require('./cookies'),
+    vars: require('./reqVars')
 };
